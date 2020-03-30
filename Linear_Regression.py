976eadb919760350e12a55f019da1297cbdfe4f9
Sandip Dutta <57990650+Dutta-SD@users.noreply.github.com>
2020-03-30 13:20:45 +0530

    Update LinearRegression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,4 +1,5 @@
 # Implementation of Linear regression on SAT-GPA database
+# Linear Regression is used to predict values that follow a linear trend
 
 import pandas as pd 
 import matplotlib.pyplot as plt
@@ -10,10 +11,12 @@ df = pd.read_csv('SAT_GPA.csv')
 X = df['GPA']
 Y = df['SAT']
 
+# Convert to numpy arrays and make them into n x 1 matrices to feed into 
+# sklearn model
 X = X.values.reshape(-1, 1)
 y = Y.values.reshape(-1, 1)
 
-
+# Split the data set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 lgr = linear_model.LinearRegression()
@@ -22,8 +25,11 @@ lgr.fit(X_train, y_train)
 
 y_pred = lgr.predict(X_test)
 
+# Plot the data and the predicted values
 plt.scatter(X_test, y_test)
 plt.plot(X_test, y_pred, color = 'red')
 plt.show()
 
+# score is the r2 score used to evaluate the model. Poor r2 score
+# (closer to 0 ) indicates poor prediction
 print(lgr.score(X_test, y_test))
